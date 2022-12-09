7b158e7dd25e818f6002886c2ebe0886c375cc89
JesusAParedes <paredesjesus93@gmail.com>
2022-12-08 20:59:44 -0600

    got 2 pokemon on the page

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // require('dotenv').config();
 
-let pokemonData;
+let pokemonData1;
+let pokemonData2;
 let locArray;
 
 const button = document.createElement('button');
@@ -13,10 +14,14 @@ window.onload = () => {
 }
 
 const grabData = () => {
-    fetch('https://pokeapi.co/api/v2/pokemon/35/')
+    fetch('https://pokeapi.co/api/v2/pokemon/152/')
     .then(res => res.json())
-    .then(array => pokemonData = array)
-    console.log(pokemonData)
+    .then(array => {
+        pokemonData1 = array
+    });
+    fetch('https://pokeapi.co/api/v2/pokemon/79/')
+    .then(res => res.json())
+    .then(arr => pokemonData2 = arr)
 }
 
 
@@ -30,18 +35,23 @@ const background = () => {
 
 const backSprite = () => {
     const div = document.createElement('div');
-    const img = new Image();
-    
-        img.src = pokemonData.sprites.versions['generation-iv']['heartgold-soulsilver'].back_shiny
+    const img = document.createElement('img');
+    console.log(pokemonData1)
+    img.src = pokemonData1.sprites.versions['generation-iv']['heartgold-soulsilver'].back_shiny
     div.append(img);
-    body.append(div);
-        
+    document.body.append(div);
+    
 }
-
-button.addEventListener('onclick', backSprite());
-
 
 const frontSprite = () => {
-    pokemonData.map(sprite =>
-        sprite.sprites.versions['generation-iv']['heartgold-soulsilver'].front_shiny);
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    console.log(pokemonData2)
+    img.src = pokemonData2.sprites.versions['generation-iv']['heartgold-soulsilver'].front_shiny
+    div.append(img);
+    document.body.append(div);
 }
+
+button.addEventListener('click', frontSprite);
+button.addEventListener('click', backSprite);
+
